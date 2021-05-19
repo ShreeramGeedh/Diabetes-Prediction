19c9f012948281ab613119aedfb62eff61767371
Shreeram Geedh <72985123+ShreeramGeedh@users.noreply.github.com>
2021-05-19 12:31:50 +0530

    Update Diabetes Predictor - Deployment.py

--- Diabetes Predictor - Deployment.py
+++ Diabetes Predictor - Deployment.py
@@ -27,7 +27,7 @@ y = df['Outcome']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
 # Creating Random Forest Model
-from sklearn.ensemble._forest import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators=20)
 classifier.fit(X_train, y_train)
 
